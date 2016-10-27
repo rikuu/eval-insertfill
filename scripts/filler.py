78d72dd7b6009528e660943be6a3d43a6187d30d
Riku Walve <riku.walve@gmail.com>
2016-10-27 13:34:43 +0300

    add vcf cutting to filler.py

--- scripts/filler.py
+++ scripts/filler.py
@@ -35,6 +35,7 @@ class Gap:
             '-length', str(self.length)]
 
 # Callback function to gather statistics on gaps
+# TODO: Report some progress while running
 filled_gaps = []
 def count_filled(result):
     global filled_gaps
@@ -48,7 +49,7 @@ def fill_gap(libraries, gap, k, fuz, solid, threshold, max_mem):
     # Extract reads
     with open('tmp.extract.' + gap.id + '.log', 'w') as f:
         unmapped = (threshold != -1)
-        exact = 1 # Exact is currently faster and more accurate
+        exact = 1 # Exact is faster and more accurate
 
         extract_params = '%i %i %i' % (exact, unmapped, threshold)
         for lib in libraries:
@@ -82,9 +83,14 @@ def fill_gap(libraries, gap, k, fuz, solid, threshold, max_mem):
         'tmp.reads.' + gap.id + '.fasta',
         'tmp.reads.' + gap.id + '.h5'])
 
+    # Remove logs
+    subprocess.check_call(['rm', '-f',
+        'tmp.extract.' + gap.id + '.log',
+        'tmp.gap2seq.' + gap.id + '.log'])
+
     return (filled, gap.comment, fill)
 
-# NOTE: Assumes gaps and the bed file are both sorted by position
+# NOTE: Assumes gaps and the bed file are in the same order
 def parse_gap(bed, gap, id, flank_length):
     gap = gap.split('\n')
     comment = gap[0]
@@ -150,11 +156,15 @@ def join_filled(out='filled.fasta'):
             f.write(comment + '\n' + fill + '\n')
     return num_success
 
+# Run GapCutter, i.e. cut scaffolds into contigs and gaps
 def cut_gaps(scaffolds):
+    # TODO: Take these from some input?
     contigs_file = 'tmp.contigs'
     gap_file = 'tmp.gaps'
     bed_file = 'tmp.bed'
 
+    # Delete the files without warning to make sure they don't exist
+    # TODO: Check if the files exist instead!
     subprocess.check_call(['rm', '-f', contigs_file, gap_file, bed_file])
 
     subprocess.check_call([gapcutter,
@@ -166,6 +176,7 @@ def cut_gaps(scaffolds):
 
     return open(bed_file, 'r'), open(gap_file, 'r')
 
+# Run GapMerger, i.e. merge contigs and filled gaps back into scaffolds
 def merge_gaps(filled, merged):
     contigs_file = 'tmp.contigs'
 
@@ -177,25 +188,83 @@ def merge_gaps(filled, merged):
 
     # subprocess.check_call(['rm', '-f', contigs_file, filled])
 
+# Parse VCF file and extract kmers from reference genome
+# TODO: Import function from a script, maybe?
+def cut_vcf(vcf, reference_file, k, fuz):
+    # TODO: Don't hardcode these
+    contigs_file = 'tmp.contigs'
+    gap_file = 'tmp.gaps'
+    bed_file = 'tmp.bed'
+
+    # Parse chromosomes from the reference into a dictionary
+    # NOTE: Since this loads the entire reference genome into RAM, we will
+    # expect the genome to be short enough.
+    reference = {}
+    comment, seq = '', ''
+    for line in f:
+        if line[0] == '>':
+            if seq != '':
+                reference[comment] = seq
+                seq = ''
+            comment = line.rstrip()[1:]
+        seq += line.rstrip()
+    reference[comment] = seq
+
+    # Open a new gap and bed files
+    gap, bed = open(gap_file, 'r+'), open(bed_file, 'r+')
+    for line in f:
+        if line[0] == '#': continue
+        fields = line.rstrip().split('\t')
+
+        # Parse VCF fields
+        insert = fields[4]
+        comment, start, end = fields[0], int(fields[1]) - 1, int(fields[1]) + len(insert) - 1
+
+        # Extract kmers from reference seqeuences
+        left = reference[comment][start:start+k+fuz]
+        right = reference[comment][end-(k+fuz):end]
+        seq = left + 'N' * len(insert) + right
+
+        gap.write('>%s:%i-%i\n%s' % (comment, start, end, seq))
+        bed.write('%s\t%i\t%i' % (comment, start-(k+fuz)+1, end+(k+fuz)+1))
+
+    # We will read the files later, so they need to be seeked to beginning
+    gap.seek(0)
+    bed.seek(0)
+
+    return gap, bed
+
 if __name__ == '__main__':
     import argparse
+    parser = argparse.ArgumentParser(description='Gap2Seq with optional read filtering.')
 
-    parser = argparse.ArgumentParser(description='Run Gap2Seq with read filtering.')
+    # filler.py specific arguments
     parser.add_argument('-o', '--out', type=str, default='filled.fasta')
-    parser.add_argument('-l', '--libraries', required=True)
-    parser.add_argument('-i', '--index', type=int, default=-1)
     parser.add_argument('-t', '--threads', type=int, default=1)
-    parser.add_argument('-u', '--unmapped-threshold', type=int, default=25)
 
+    # Gap2Seq specific arguments
     parser.add_argument('-k', type=int, default=31)
     parser.add_argument('--fuz', type=int, default=10)
     parser.add_argument('--solid', type=int, default=2)
     parser.add_argument('--max-mem', type=int, default=20)
 
+    # Either a set of mapped read libraries or a set of fasta formatted reads
+    # TODO: Hide -i option, needless confusion
+    parser.add_argument('-l', '--libraries')
+    parser.add_argument('-i', '--index', type=int, default=-1)
+    parser.add_argument('-u', '--unmapped-threshold', type=int, default=25)
+
+    # One of three options is required for gap data:
+    # 1. Cut gaps and bed from some scaffolds
     parser.add_argument('-s', '--scaffolds', type=str)
 
+    # 2. Use pre-cut gaps and bed
     parser.add_argument('-b', '--bed', type=argparse.FileType('r'))
     parser.add_argument('-g', '--gaps', type=argparse.FileType('r'))
+
+    # 3. Generate gaps and bed from VCF
+    parser.add_argument('-v', '--vcf', type=argparse.FileType('r'))
+    parser.add_argument('-r', '--reference', type=argparse.FileType('r'))
 
     args = vars(parser.parse_args())
 
@@ -216,9 +285,12 @@ if __name__ == '__main__':
             print('Cutting gaps')
             args['bed'], args['gaps'] = cut_gaps(args['scaffolds'])
             scaffolds_cut = True
+        elif args['vcf'] != None:
+            print('Parsing VCF')
+            args['bed'], args['gaps'] = cut_vcf(args['vcf'], args['reference'], args['k'], args['fuz'])
         else:
             parser.print_help()
-            print('Either [-b/--bed and -g/--gaps] or [-s/--scaffolds] are required.')
+            print('Either [-b/--bed and -g/--gaps], [-v/--vcf and -r/--reference], or [-s/--scaffolds] are required.')
             exit(1)
 
     pool = multiprocessing.Pool(args['threads'])
